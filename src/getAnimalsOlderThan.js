ded5e808b9951fcb959e6817d838661d38462d39
Luis Monte <luismonte.l560@gmail.com>
2021-11-24 17:27:14 -0300

    respondo o requisito 02 - function getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  for (let i = 0; i < species.length; i += 1) {
+    if (species[i].name === animal) {
+      return species[i].residents.every((teste) => teste.age > age);
+    }
+  }
 }
 
 module.exports = getAnimalsOlderThan;
