64491c9b18beee857908c55444eb7acbd13efa8d
Luis Monte <luismonte.l560@gmail.com>
2021-11-24 18:16:05 -0300

    Resposta do requisito 03 - function getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  let funcionario = {};
+  for (let i = 0; i < employees.length; i += 1) {
+    if (employees[i].firstName === employeeName) {
+      funcionario = employees.filter((teste) => teste.firstName === employeeName);
+      return funcionario[0];
+    }
+    if (employees[i].lastName === employeeName) {
+      funcionario = employees.filter((teste) => teste.lastName === employeeName);
+      return funcionario[0];
+    }
+  }
+  return funcionario;
 }
 
 module.exports = getEmployeeByName;
