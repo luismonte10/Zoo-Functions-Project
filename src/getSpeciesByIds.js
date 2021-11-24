978c274d0caf4166793189dda0cab76b45efefd5
Luis Monte <luismonte.l560@gmail.com>
2021-11-24 17:42:44 -0300

    Resposta requisito 01 - function getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const animal = [];
+  const sera = [...ids];
+  if (ids === undefined) return animal;
+  for (let i = 0; i < sera.length; i += 1) {
+    animal.push(species.find((teste) => teste.id === sera[i]));
+  }
+  return animal;
 }
 
 module.exports = getSpeciesByIds;
