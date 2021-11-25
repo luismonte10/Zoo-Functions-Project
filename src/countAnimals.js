9c08ad7dc9e5ca95657265ba5aa23e213117a12e
Luis Monte <luismonte.l560@gmail.com>
2021-11-25 04:59:39 -0300

    Resposta do requisito 05 - function countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((accAnimals, animals) => Object.assign(accAnimals, {
+      [animals.name]: animals.residents.length }), {});
+  }
+  const teste = species.find((ani) => animal.specie === ani.name);
+  if (Object.keys(animal).length === 1) return teste.residents.length;
+  return teste.residents.filter((tes) => tes.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
