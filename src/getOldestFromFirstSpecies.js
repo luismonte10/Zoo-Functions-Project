d2dd7cad77ad7d6572f6239d63713b6e7926b00b
Luis Monte <luismonte.l560@gmail.com>
2021-11-25 16:54:25 -0300

    Resposta do requisito 09 - function getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employeeById = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animal = species.find((ani) => ani.id === employeeById).residents;
+  const animalByAge = animal.sort((a, b) => b.age - a.age);
+  return [animalByAge[0].name, animalByAge[0].sex, animalByAge[0].age];
 }
+
+getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83');
 
 module.exports = getOldestFromFirstSpecies;
