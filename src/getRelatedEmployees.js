01e52efbbdc0257f3b520b0c1ce03da84018efcf
Luis Monte <luismonte.l560@gmail.com>
2021-11-24 22:59:15 -0300

    Resposta do requisito 04 - function getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return employees.some((gerente) => gerente.managers.find((teste) => teste === id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const test = employees.filter((employee) => employee.managers.includes(managerId));
+    return test.map((fullName) => `${fullName.firstName} ${fullName.lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
